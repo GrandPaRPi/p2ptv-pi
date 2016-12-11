6f79c00f9055a3763ddfe1dc41e14d2cb533f4c3
GrandPaRPi <grandpa.rpi@outlook.es>
2016-12-11 20:08:41 +0100

    Update overlay.sh

--- overlay.sh
+++ overlay.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-printf "$1"|osd_cat --font="-adobe-helvetica-bold-r-normal--34-240-100-100-p-182-iso8859-1" --color='brown' --align='left' --indent='140' --delay='5' --shadow='1' --pos='top' --offset='40'
+printf "CH $1"|osd_cat --font="-adobe-helvetica-bold-r-normal--34-240-100-100-p-182-iso8859-1" --color='brown' --align='left' --indent='140' --delay='5' --shadow='1' --pos='top' --offset='40'
